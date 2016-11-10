22b6d15de8658d1379beb4c4ed45e47a3cb9075d
jpcik <jean.paul.ik@gmail.com>
2016-11-10 17:57:58 +0100

    changed header added index collapsable

--- assets/toc.js
+++ assets/toc.js
@@ -1,34 +1,32 @@
 // https://github.com/ghiculescu/jekyll-table-of-contents
 
 function romanize (num) {
-    if (!+num)
-        return false;
-    var digits = String(+num).split(""),
-        key = ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM",
-               "","X","XX","XXX","XL","L","LX","LXX","LXXX","XC",
-               "","I","II","III","IV","V","VI","VII","VIII","IX"],
-        roman = "",
-        i = 3;
-    while (i--)
-        roman = (key[+digits.pop() + (i * 10)] || "") + roman;
-    return Array(+digits.join("") + 1).join("M") + roman;
+  if (!+num)
+    return false;
+  var digits = String(+num).split(""),
+      key = ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM",
+             "","X","XX","XXX","XL","L","LX","LXX","LXXX","XC",
+             "","I","II","III","IV","V","VI","VII","VIII","IX"],
+      roman = "",
+      i = 3;
+  while (i--)
+    roman = (key[+digits.pop() + (i * 10)] || "") + roman;
+ return Array(+digits.join("") + 1).join("M") + roman;
 }
 
 function chap(innerText,cnt,level){
-
- //console.log(level);
-    if (innerText == "&nbsp;") 
-      innerText=" "+romanize(cnt)+".";
-    else if (level !=2.5 && level!=1) innerText=romanize(cnt)+". "+innerText;
-   return innerText;
+  //console.log(level);
+  if (innerText == "&nbsp;") 
+    innerText=" "+romanize(cnt)+".";
+  else if (level !=2.5 && level!=1) innerText=romanize(cnt)+". "+innerText;
+  
+  return innerText;
 }
 
 
-      
-
+     
 
 $(document).ready(function() {
-
 
   var no_back_to_top_links = true
 
@@ -37,9 +35,13 @@ $(document).ready(function() {
   if (!headers.length || headers.length < 3 || !output.length)
     return;
 
-  var get_level = function(ele) { console.log(ele.nodeName);
-if (ele.nodeName=="P") return 2.5;
-else return parseInt(ele.nodeName.replace("H", ""), 10) }
+  var get_level = function(ele) { 
+    console.log(ele.nodeName);
+  
+    if (ele.nodeName=="P") return 2.5;
+    else return parseInt(ele.nodeName.replace("H", ""), 10) 
+  }
+
   var highest_level = headers.map(function(_, ele) { return get_level(ele) }).get().sort()[0]
   var return_to_top = '<i class="icon-arrow-up back-to-top"> </i>'
 
@@ -64,12 +66,14 @@ else return parseInt(ele.nodeName.replace("H", ""), 10) }
     
       if (level!=2.5)
         cnts[this_level]=1;
-      if (this_level==3) dl=dl+" class='columnList' "
+      if (this_level==3) 
+        dl=dl+" class='columnList' "
       html += dl+"><dd><a class='links' href='#" + header.id + "'>" + chap(header.innerHTML,cnts[this_level],this_level) + "</a>";
     }
     level = this_level; // update for the next one
     cnts[level]++;
   });
+
   html += "</dl>";
   if (!no_back_to_top_links) {
     $(document).on('click', '.back-to-top', function() {
@@ -78,40 +82,42 @@ else return parseInt(ele.nodeName.replace("H", ""), 10) }
     })
   }
   output.hide().html(html).show('slow');
-$('article.group').prepend("<div class='mini'>-</div>");
-$('.toc').addClass("maximize");
+
+  $('article.group').prepend("<div class='mini'>Index</div>");
+ // $('.toc').addClass("minimize");
+
   $('.part').addClass("hidd");
 
-        $('.links').click(function() {    
+  $('.links').click(function() {    
         
-            var $this = $(this);
-            var id=$(this).attr('href')
-console.log(id.startsWith("#nbsp"))
-if (!id.startsWith("#nbsp")){
-  $('.part:not('+id+')').removeClass("viss").addClass("hidd");
+     var $this = $(this);
+     var id=$(this).attr('href')
+     console.log(id.startsWith("#nbsp"))
+     if (!id.startsWith("#nbsp")){
+       $('.part:not('+id+')').removeClass("viss").addClass("hidd");
+       var pip=$(id)
 
-            var pip=$(id)
-
-            if (pip.hasClass("hidd")) {
-                pip.removeClass("hidd").addClass("viss");
-
-            } else {
-                pip.removeClass("viss").addClass("hidd");
-            }
-}
-
-        });
+       if (pip.hasClass("hidd")) {
+         pip.removeClass("hidd").addClass("viss");
+       } 
+       else {
+         pip.removeClass("viss").addClass("hidd");
+       }
+     }
+   });
 
 
-      $('.mini').click(function() {
-          var $this=$('.toc');
-          if ($this.hasClass("minimize")){
-  $this.removeClass("minimize").addClass("maximize");
+   $('.mini').click(function() {
+//  $('.toc').css('display','none');
+      console.log('pipas')
+      var $this=$('.toc');
+      if ($this.css('display')=='none'){
+        $this.css('display','block');
+      } 
+      else {
+        $this.css('display','none');
+      }
 
-            } else {
-                $this.removeClass("maximize").addClass("minimize");
-            }
-
-      });
+   });
 
 });
